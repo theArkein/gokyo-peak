27456374f0fefd66d1a9f7f1ea49d011bbe683b6
Sarad Singh <theArkein.dev@gmail.com>
2021-02-15 14:57:31 +0545

    Added Recomendation in Product Page, Responsive forms

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("working")
-
 const menu = document.querySelector('nav')
 const hamburger = document.querySelector('.hamburger')
 
@@ -30,3 +28,133 @@ function showTab (event,name){
      newTab.classList.add('show')
 }
 
+$(document).ready(function(){
+          $(".banner.owl-carousel").owlCarousel({
+               loop:true,
+               items:1,
+               nav:true,
+               dots:true,
+               dotsContainer: '.dots',
+               navContainer: '.nav',
+               autoplay: true,
+               autoplayTimeout: 5000,
+               autoplayHoverPause: true,
+          });
+          let couponCarousel = {
+               class: ".hot-deals", 
+               options: {
+                    loop:true,
+                    nav:true,
+                    dots:true,
+                    autoWidth: true,
+                    margin: 35
+               }
+          }
+          let destinationsCarousel = {
+               class: ".popular-destinations", 
+               options: {
+                    loop:true,
+                    nav:true,
+                    dots:true,
+                    autoWidth: true,
+                    margin: 35
+               }
+          }
+          let adventureCarousel = {
+               class: ".exciting-adventures", 
+               options: {
+                    loop:true,
+                    nav:true,
+                    dots:true,
+                    autoWidth: true,
+                    margin: 35
+               }
+          }
+          let clientReviewsCarousel = {
+               class: ".client-reviews", 
+               options: {
+                    loop:true,
+                    items:1,
+                    nav:true,
+                    dots:true,
+                    margin: 20
+               }
+          }
+          let trendingTrips = {
+               class: ".trending-trips", 
+               options: {
+                    loop:true,
+                    nav:true,
+                    autoWidth: true,
+                    margin: 35
+               }
+          }
+
+          let carousels = [couponCarousel, destinationsCarousel, adventureCarousel, clientReviewsCarousel, trendingTrips]
+
+          carousels.forEach(item=>{
+               let carousel = $(`${item.class} .owl-carousel`)
+               carousel.owlCarousel(item.options);
+               carousel.on('mousewheel', '.owl-stage', function (e) {
+               if (e.deltaY<0) {
+                    carousel.trigger('next.owl');
+               } else {
+                    carousel.trigger('prev.owl');
+               }
+               e.preventDefault();
+          });
+          })
+     });
+
+ function showFormStep (event,name){
+          let processStep = event.currentTarget;
+          document.querySelectorAll('.process-step').forEach(item=>{
+             item.classList.remove('active')
+         })
+          processStep.classList.add('active')
+         document.querySelectorAll('.form-steps').forEach(item=>{
+             item.classList.remove('active')
+         })
+         const formSteps = document.querySelectorAll('.form-steps .form-step')
+         const showStep = document.querySelector('.'+name)
+         formSteps.forEach(element => {
+             element.classList.remove('show')
+         });
+             showStep.classList.add('show')
+
+     }
+
+function showPopUp(name){
+     let popUp = document.querySelector(`.pop-up.${name}`)
+     document.querySelectorAll(`.pop-up`).forEach(element=>{
+          element.classList.remove('show')
+
+     })
+     popUp.classList.add('show')
+}
+
+const hidePopUp = (name)=>{
+     let popUp = document.querySelector(`.pop-up.${name}`)
+     console.log(popUp)
+     popUp.classList.remove('show')
+}
+
+const uploadDocument = ()=>{
+     const selectedDocument = document.getElementById("document").value;
+     const documentForm = document.querySelectorAll('.upload-document')
+     console.log(selectedDocument,documentForm)
+     
+     if(selectedDocument=="none"){
+          documentForm[0].classList.remove('show')
+          documentForm[1].classList.remove('show')
+     }
+     if(selectedDocument=="citizenship"){
+          documentForm[0].classList.add('show')
+          documentForm[1].classList.remove('show')
+     }
+     if(selectedDocument=="passport"){
+          documentForm[1].classList.add('show')
+          documentForm[0].classList.remove('show')
+     }
+}
+uploadDocument()
